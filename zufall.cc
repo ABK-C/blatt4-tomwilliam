dc6e37de744fe34ef2b07f611cb05e7da2d73684
null null <tom-hornberger@gmx.de>
2020-05-27 10:38:19 +0000

    Tom

--- zufall.cc
+++ zufall.cc
@@ -1,2 +1,24 @@
+#include<cmath>
+
+
+
+
 int main() {
+
+
+  double zufall(){
+    static int I = 1;
+    int a = 5;
+    int c = 3;
+    int m = 16;
+
+    I = (a*I+c)mod(m);
+
+    double z = I/m;
+
+    return z;
+  }
+
+  for()
+
 }
