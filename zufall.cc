d926261359e3dae52d03471f2072b054f2c7db27
Buzzlez <64007187+Buzzlez@users.noreply.github.com>
2020-05-27 16:05:23 +0200

    Blatt 4

--- zufall.cc
+++ zufall.cc
@@ -1,24 +1,62 @@
 #include<cmath>
+#include<iostream>
+#include <iomanip>
+
+double zufall(int old,int a, int c, int m){
+    static int I;
+    I = (a*old+c)%(m);
 
 
+    return I;
+  }
 
 
 int main() {
+  using namespace std;
 
+  float r = 689;
+  float ry = 312;
+  float x = r;
+  float y = ry;
+  float treffer = 0;
+  float zaehler = 100000;
+  float a = 205;
+  float c = 29573;
+  float m = 139968;
+  
+  for(int N = 0; N < zaehler; N++){
+    
+    r = zufall(r, a, c, m);
+    ry = zufall(ry, a, c, m);
+    float zy = ry/m;
+    float z = r/m;
 
-  double zufall(){
-    static int I = 1;
-    int a = 5;
-    int c = 3;
-    int m = 16;
+    float rad = sqrt((z*z)+(zy*zy));
+    if(rad < 1){
+      treffer++;
+    }
 
-    I = (a*I+c)mod(m);
-
-    double z = I/m;
-
-    return z;
+    //cout <<"X: "<< z <<" Y: " << zy << " Radius: " << rad << endl;
   }
 
-  for()
+  float pi = 4*(treffer/zaehler);
+  float p = M_PI/4;
+  float var = p*(1-p)*zaehler;
+  float sig = 4*sqrt(var)/zaehler;
+  float exp = p*zaehler;
+  
+  cout << "a: " << a << endl;
+  cout << "c: " << c << endl;
+  cout << "m: " << m << endl;
+  cout << "X0: " << x << endl;
+  cout << "Y0: " << y << endl;
+  cout << "N: " << zaehler << endl;
+  cout << "Erwartungswert: " << exp << endl;
+  cout << "Varianz: " << var << endl;
+  cout << "N_in: " << treffer << endl;
+  cout << "Pi: " << pi << endl;
+  cout << "Sigma: " << sig << endl;
+  cout <<"a="<<a<<" c="<<c<<" m="<<m<<" X0="<<x<<" Y0="<<y<<" "<<zaehler<<" "<<exp<<" "<<var<<" "<<treffer<<" "<<pi<<" "<<sig<< endl;
 
+  
 }
